66a79e1b7c0d70c82891445c2028f76cd66ae7cc
uit2712 <phamquangvi456@gmail.com>
2020-02-18 02:19:33 +0700

    Updates

---
+++ precache-manifest.6e2f7b40babf98dd7264cd87b878c649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8405a12679c184816a02625043426ed",
+    "url": "/quiz-app-ts/index.html"
+  },
+  {
+    "revision": "3b4c8f680184ce709924",
+    "url": "/quiz-app-ts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "62911e7c730171b99c6a",
+    "url": "/quiz-app-ts/static/js/2.831f2e5f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quiz-app-ts/static/js/2.831f2e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4c8f680184ce709924",
+    "url": "/quiz-app-ts/static/js/main.06a7c7da.chunk.js"
+  },
+  {
+    "revision": "e41af3f26314eb5d7fed",
+    "url": "/quiz-app-ts/static/js/runtime-main.2cbb53fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz-app-ts/static/media/logo.5d5d9eef.svg"
+  }
+]);
